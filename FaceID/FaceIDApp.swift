ca8970e0e63b3e01518f7b06ffc4b03b70cf1001
Hallucinette <ameliepocchiolo42M@gmail.com>
2023-11-20 13:50:24 +0100

    Creat project

---
+++ FaceID/FaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDApp.swift
+//  FaceID
+//
+//  Created by Amelie Pocchiolo on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
